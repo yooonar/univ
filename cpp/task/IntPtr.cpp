a27af48d708f5bf0d56d597b326783eb4d20a931
yooonar <aryooon33@gmail.com>
2020-09-26 18:02:31 +0900

    출석수업 과제

--- cpp/task/IntPtr.cpp
+++ cpp/task/IntPtr.cpp
@@ -14,15 +14,10 @@ int main()
 
     int a = 10, b = 100;
     int *ptr = &a; // 포인터 ptr 을 선언한 후 a 의 주소를 넣는다
-
     cout << "ptr 이 가리키는 곳의 값 : " << *ptr << endl;
     *ptr = 20; // ptr이 가리키는 곳에 20을 저장
     cout << "변수 a의 값 : " << a << endl;
-
     ptr = &b; // ptr 이 b 를 가리키게 함
     cout << "변수 b의 값 : " << *ptr << endl;
-    delete ptr;
-    ptr = nullptr;
-
     return 0;
 }
