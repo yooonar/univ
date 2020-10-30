f3af4b3e074d96e5050dbd792f80debebae9c2a9
yooonar <aryooon33@gmail.com>
2020-10-30 15:10:57 +0900

    디폴트 생성자

--- cpp/Class/Counter/CounterM.h
+++ cpp/Class/Counter/CounterM.h
@@ -16,7 +16,10 @@ class CounterM { // 클래스 CounterM의 선언 시작
     int value; // private 데이터 멤버
 
     public: // public 멤버 함수
-    CounterM(int mVal) : maxValue {mVal}, value{0} { } // 생성자
+    CounterM(int mVal) : maxValue { mVal }, value { 0 } { } // 생성자 maxValue 데이터 멤버의 초기값을 mVal 로, value 데이터 멤버의 초기값을 0 으로 초기화하여 나열함
+    // maxValue 는 상수 멤버로 선언(const)했기 때문에 초기화 리스트를 이용해서만 초기화가 가능하다.
+    // 만약 이와 같이 CounterM(int mVal) : maxValue { mVal }, value { 0 } { maxValue = mVal; } 로 생성자 안에서 대입했다면 에러가 발생한다.(maxValue 가 상수 인자(const) 이기 때문에)
+    
     void reset() // 계수기의 값을 0으로 지움
     {
         value = 0;
